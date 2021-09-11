cdc05a3f1ffd2a5e7528c57b8ef4c0a85f3fb363
SkyG0D <silvanosilvino@hotmail.com>
2021-09-11 02:41:16 -0300

    :recycle: Carousel project refactored

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,52 +1,86 @@
-function carousel() {
-    const images = document.querySelectorAll('.carousel__item');
-    const next = document.querySelector('.carousel__control--next');
-    const previous = document.querySelector('.carousel__control--previous');
+/**
+ * @typedef CarouselType
+ * @property {HTMLElement[]} images Elementos HTML para manipular.
+ * @property {HTMLElement} btnNext Elemento HTML que ao clicar altera para o próximo.
+ * @property {HTMLElement} btnPrevious Elemento HTML que ao clicar altera para o anterior.
+ * @property {number} switchTime Tempo que demora para passar cada elemento(em millisegundos).
+ * @property {string} visibleCSSClass Classe CSS que é adicionado para o elemento visiível.
+ * @property {boolean} reverseMode Carrosel vai funcionar de trás pra frente caso seja passado **true**.
+ */
 
-    const SWITCH_TIME = 5000;
+/**
+ * @param {CarouselType} {
+ * @param {HTMLElement[]} images=[] Elementos HTML para manipular.
+ * @param {HTMLElement} btnNext Elemento HTML que ao clicar altera para o próximo.
+ * @param {HTMLElement} btnPrevious Elemento HTML que ao clicar altera para o anterior.
+ * @param {number} switchTime = 5000 Tempo que demora para passar cada elemento(em millisegundos).
+ * @param {string} visibleCSSClass Classe CSS que é adicionado para o elemento visiível.
+ * @param {boolean} reverseMode = false Carrosel vai funcionar de trás pra frente caso seja passado **true**.
+ * @param {} }
+ */
+function carousel({
+  children = [],
+  btnNext,
+  btnPrevious,
+  switchTime = 5000,
+  visibleCSSClass,
+  reverseMode = false,
+}) {
+  let currentIndex = -1;
+  let timerID;
 
-    let currentIndex = -1;
-    let nextImageId;
+  function resetTimer() {
+    clearInterval(timerID);
 
-    function nextImage() {
-        if (currentIndex === images.length - 1) {
-            currentIndex = -1;
-        }
-        currentIndex++;
+    const func = reverseMode ? previousImage : nextImage;
 
-        images.forEach(image =>
-            image.classList.remove('carousel__item--visible'));
+    timerID = setInterval(func, switchTime);
+  }
 
-        images[currentIndex].classList.add('carousel__item--visible');
+  function handleCSSClases() {
+    children.forEach(image => image.classList.remove(visibleCSSClass));
+    children[currentIndex].classList.add(visibleCSSClass);
+  }
+
+  function nextImage() {
+    resetTimer();
+
+    if (currentIndex === children.length - 1) {
+      currentIndex = -1;
     }
 
-    function previousImage() {
-        if (currentIndex <= 0) {
-            currentIndex = images.length;
-        }
-        currentIndex--;
+    currentIndex++;
 
-        images.forEach(image =>
-            image.classList.remove('carousel__item--visible'));
+    handleCSSClases();
+  }
 
-        images[currentIndex].classList.add('carousel__item--visible');
+  function previousImage() {
+    resetTimer();
+
+    if (currentIndex <= 0) {
+      currentIndex = children.length;
     }
 
-    function resetTimer() {
-        clearInterval(nextImageId);
-        nextImageId = setInterval(nextImage, SWITCH_TIME);  
-    }
+    currentIndex--;
 
-    next.addEventListener('click', () => {
-        resetTimer();
-        nextImage();
-    });
-    previous.addEventListener('click', () => {
-        resetTimer();
-        previousImage();
-    });
+    handleCSSClases();
+  }
 
-    nextImageId = setInterval(nextImage, SWITCH_TIME);
+  btnNext.addEventListener('click', nextImage);
+  btnPrevious.addEventListener('click', previousImage);
+
+  resetTimer();
 }
 
-carousel();
+const images = document.querySelectorAll('.carousel__item');
+const btnNext = document.querySelector('.carousel__control--next');
+const btnPrevious = document.querySelector('.carousel__control--previous');
+
+carousel({
+  children: images,
+  btnNext,
+  btnPrevious,
+  switchTime: 1000,
+  visibleCSSClass: 'carousel__item--visible',
+  reverseMode: true,
+});
